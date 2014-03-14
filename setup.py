9cc7622af1cc5399fb46e0c05c23888574a2734b
robintibor <robintibor@gmail.com>
2014-03-14 13:58:19 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='mindwavemobile',
       version='0.1',
-      description='The funniest joke in the world',
+      description='Small Library to access neurosky mindwave mobile functionality from python',
       url='https://github.com/robintibor/python-mindwave-mobile',
       author='Robin Tibor Schirrmeister',
       author_email='robintibor@gmail.com',
