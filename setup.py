0abb91d5423eed0b6d52dfd157880ff2af92acae
robintibor <robintibor@gmail.com>
2015-01-03 13:16:50 +0100

    incremented version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='mindwavemobile',
-      version='0.1',
+      version='0.200',
       description='Small Library to access neurosky mindwave mobile functionality from python',
       url='https://github.com/robintibor/python-mindwave-mobile',
       author='Robin Tibor Schirrmeister',
