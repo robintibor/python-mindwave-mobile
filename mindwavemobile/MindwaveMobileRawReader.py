f2bff98ac87cf5fdc9f1e036947efa1b0866793e
BioMycoBit <5253282+BioMycoBit@users.noreply.github.com>
2019-03-04 16:52:45 -0600

    Update MindwaveMobileRawReader.py
    py2 -> py3 byte handling required changes.

--- mindwavemobile/MindwaveMobileRawReader.py
+++ mindwavemobile/MindwaveMobileRawReader.py
@@ -2,15 +2,15 @@ import bluetooth
 import time
 import textwrap
 
+
 class MindwaveMobileRawReader:
     START_OF_PACKET_BYTE = 0xaa;
-
     def __init__(self, address=None):
         self._buffer = [];
-        self._bufferPosition = 0
-        self._isConnected = False
+        self._bufferPosition = 0;
+        self._isConnected = False;
         self._mindwaveMobileAddress = address
-
+        
     def connectToMindWaveMobile(self):
         # First discover mindwave mobile address, then connect.
         # Headset address of my headset was'9C:B7:0D:72:CD:02';
@@ -18,19 +18,19 @@ class MindwaveMobileRawReader:
         # now discovering address because of https://github.com/robintibor/python-mindwave-mobile/issues/4
         if (self._mindwaveMobileAddress is None):
             self._mindwaveMobileAddress = self._findMindwaveMobileAddress()
-        if (self._mindwaveMobileAddress is not None):
-            print("Discovered Mindwave Mobile...")
+        if (self._mindwaveMobileAddress is not None):            
+            print ("Discovered Mindwave Mobile...")
             self._connectToAddress(self._mindwaveMobileAddress)
         else:
             self._printErrorDiscoveryMessage()
-
+        
     def _findMindwaveMobileAddress(self):
-        nearby_devices = bluetooth.discover_devices(lookup_names=True)
+        nearby_devices = bluetooth.discover_devices(lookup_names = True)
         for address, name in nearby_devices:
             if (name == "MindWave Mobile"):
                 return address
         return None
-
+        
     def _connectToAddress(self, mindwaveMobileAddress):
         self.mindwaveMobileSocket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
         while (not self._isConnected):
@@ -40,13 +40,14 @@ class MindwaveMobileRawReader:
                 self._isConnected = True
             except bluetooth.btcommon.BluetoothError as error:
                 print("Could not connect: ", error, "; Retrying in 5s...")
-                time.sleep(5)
+                time.sleep(5) 
+           
 
     def isConnected(self):
         return self._isConnected
 
     def _printErrorDiscoveryMessage(self):
-        print((textwrap.dedent("""\
+         print((textwrap.dedent("""\
                     Could not discover Mindwave Mobile. Please make sure the
                     Mindwave Mobile device is in pairing mode and your computer
                     has bluetooth enabled.""").replace("\n", " ")))
@@ -54,20 +55,18 @@ class MindwaveMobileRawReader:
     def _readMoreBytesIntoBuffer(self, amountOfBytes):
         newBytes = self._readBytesFromMindwaveMobile(amountOfBytes)
         self._buffer += newBytes
-
+    
     def _readBytesFromMindwaveMobile(self, amountOfBytes):
         missingBytes = amountOfBytes
         # receivedBytes = ""  #py2
-        receivedBytes = b''	  #py3
-
+        receivedBytes = b''   #py3
+        
         # Sometimes the socket will not send all the requested bytes
         # on the first request, therefore a loop is necessary...
-        while (missingBytes > 0):
-
+        while(missingBytes > 0):
             receivedBytes += self.mindwaveMobileSocket.recv(missingBytes)
-
             missingBytes = amountOfBytes - len(receivedBytes)
-        return receivedBytes
+        return receivedBytes;
 
     def peekByte(self):
         self._ensureMoreBytesCanBeRead();
@@ -75,33 +74,33 @@ class MindwaveMobileRawReader:
 
     def getByte(self):
         self._ensureMoreBytesCanBeRead(100);
-        return self._getNextByte()
-
-    def _ensureMoreBytesCanBeRead(self, amountOfBytes):
+        return self._getNextByte();
+    
+    def  _ensureMoreBytesCanBeRead(self, amountOfBytes):
         if (self._bufferSize() <= self._bufferPosition + amountOfBytes):
             self._readMoreBytesIntoBuffer(amountOfBytes)
-
+    
     def _getNextByte(self):
         # nextByte = ord(self._buffer[self._bufferPosition]) #py2
-        nextByte = self._buffer[self._bufferPosition]	#py3
-        self._bufferPosition += 1
+        nextByte = self._buffer[self._bufferPosition]   #py3
+        self._bufferPosition += 1;
         return nextByte;
 
     def getBytes(self, amountOfBytes):
-        self._ensureMoreBytesCanBeRead(amountOfBytes)
-        return self._getNextBytes(amountOfBytes)
-
+        self._ensureMoreBytesCanBeRead(amountOfBytes);
+        return self._getNextBytes(amountOfBytes);
+    
     def _getNextBytes(self, amountOfBytes):
         # nextBytes = list(map(ord, self._buffer[self._bufferPosition: self._bufferPosition + amountOfBytes])) #py2
         nextBytes = list(self._buffer[self._bufferPosition: self._bufferPosition + amountOfBytes]) #py3
         self._bufferPosition += amountOfBytes
         return nextBytes
-
+    
     def clearAlreadyReadBuffer(self):
-        self._buffer = self._buffer[self._bufferPosition:]
-        self._bufferPosition = 0
-
+        self._buffer = self._buffer[self._bufferPosition : ]
+        self._bufferPosition = 0;
+    
     def _bufferSize(self):
-        return len(self._buffer)
-
-# ------------------------------------------------------------------------------
+        return len(self._buffer);
+    
+#------------------------------------------------------------------------------ 
